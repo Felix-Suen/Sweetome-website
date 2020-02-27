e36044d195d3848b84e1964ba5200463567bf739
Felix-Suen <felix.suen2000@gmail.com>
2020-02-26 18:07:09 -0800

    about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './custom.css';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 class About extends React.Component {
     render() {
         return (
             <section className="about" id="about">
-                <h1>Our Journey</h1>
-                <p>Stress free property management</p>
+                <ScrollAnimation animateIn="fadeIn">
+                    <h1>Our Journey</h1>
+                    <p>Stress free property management</p>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeIn" delay={1000}>
+                    <div className="textbox">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </div>
+                </ScrollAnimation>
             </section>
         );
     }
