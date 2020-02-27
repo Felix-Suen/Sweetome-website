87d08eaa7c34747a0a7053b797b2849dd6eecbc0
Felix <felix.suen2000@gmail.com>
2020-02-27 14:22:36 -0800

    new cards

--- src/components/about.js
+++ src/components/about.js
@@ -6,11 +6,11 @@ class About extends React.Component {
     render() {
         return (
             <section className="about" id="about">
-                <ScrollAnimation animateIn="fadeIn">
+                <ScrollAnimation animateIn="fadeIn" animateOnce='true'>
                     <h1>Our Journey</h1>
                     <p>Stress free property management</p>
                 </ScrollAnimation>
-                <ScrollAnimation animateIn="fadeIn" delay={500}>
+                <ScrollAnimation animateIn="fadeIn" delay={500} animateOnce='true'>
                     <div className="textbox">
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
--- src/components/agent.js
+++ src/components/agent.js
@@ -1,53 +1,51 @@
 import React from 'react';
 import './custom.css';
-import { Card, CardGroup } from 'react-bootstrap';
+import { Card, CardDeck } from 'react-bootstrap';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 class Agent extends React.Component {
     render() {
         return (
             <div className="agent" id="agent">
-                <h1>Our Agents</h1><br />
-                <CardGroup>
-                    <Card>
-                        <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png"/>
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
+                <ScrollAnimation animateIn="fadeIn" animateOnce='true'>
+                    <h1>Our Trusted Agents</h1>
+                </ScrollAnimation>
+                <div className="container">
+                <ScrollAnimation animateIn="bounceInLeft" animateOnce='true' delay={500}>
+                    <CardDeck className="card-deck">
+                        <Card className="card">
+                            <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png" />
+                            <Card.Body style={{ background: '#ff6200' }}>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text style={{ color: '#fff' }}>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
                             </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png"/>
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This card has supporting text below as a natural lead-in to additional content.{' '}
+                            </Card.Body>
+                        </Card>
+                        <Card className="card">
+                            <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png" />
+                            <Card.Body style={{ background: '#ff6200' }}>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text style={{ color: '#fff' }}>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
                             </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png"/>
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This card has even longer content than the first to
-                                show that equal height action.
+                            </Card.Body>
+                        </Card>
+                        <Card className="card">
+                            <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png" />
+                            <Card.Body style={{ background: '#ff6200' }}>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text style={{ color: '#fff' }}>
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
                             </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                </CardGroup>
+                            </Card.Body>
+                        </Card>
+                    </CardDeck>
+                    </ScrollAnimation>
+                </div>
             </div>
         );
     }
