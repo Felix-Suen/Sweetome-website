5c6bc01f42c89d6e8504d0cf2ef16db77b07d2d3
Felix <felix.suen2000@gmail.com>
2020-02-27 16:09:29 -0800

    agent row

--- src/components/agent.js
+++ src/components/agent.js
@@ -11,45 +11,47 @@ class Agent extends React.Component {
                     <h1>Our Trusted Agents</h1>
                 </ScrollAnimation>
                 <div className="container">
-                <ScrollAnimation animateIn="bounceInLeft" animateOnce='true' delay={500}>
-                    <CardDeck className="card-deck">
-                        <div class="col-sm-4">
-                            <Card className="card">
-                                <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png" />
-                                <Card.Body style={{ background: '#ff6200' }}>
-                                    <Card.Title>Card title</Card.Title>
-                                    <Card.Text style={{ color: '#fff' }}>
-                                        This is a wider card with supporting text below as a natural lead-in to
-                                        additional content. This content is a little bit longer.
+                    <ScrollAnimation animateIn="bounceInLeft" animateOnce='true' delay={500}>
+                        <CardDeck className="card-deck">
+                            <div className="row">
+                                <div className="col-sm-4">
+                                    <Card className="card">
+                                        <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png" />
+                                        <Card.Body style={{ background: '#ff6200' }}>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text style={{ color: '#fff' }}>
+                                                This is a wider card with supporting text below as a natural lead-in to
+                                                additional content. This content is a little bit longer.
                                 </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <div class="col-sm-4">
-                            <Card className="card">
-                                <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png" />
-                                <Card.Body style={{ background: '#ff6200' }}>
-                                    <Card.Title>Card title</Card.Title>
-                                    <Card.Text style={{ color: '#fff' }}>
-                                        This is a wider card with supporting text below as a natural lead-in to
-                                        additional content. This content is a little bit longer.
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                                <div className="col-sm-4">
+                                    <Card className="card">
+                                        <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png" />
+                                        <Card.Body style={{ background: '#ff6200' }}>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text style={{ color: '#fff' }}>
+                                                This is a wider card with supporting text below as a natural lead-in to
+                                                additional content. This content is a little bit longer.
                                 </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                        <div class="col-sm-4">
-                            <Card className="card">
-                                <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png" />
-                                <Card.Body style={{ background: '#ff6200' }}>
-                                    <Card.Title>Card title</Card.Title>
-                                    <Card.Text style={{ color: '#fff' }}>
-                                        This is a wider card with supporting text below as a natural lead-in to
-                                        additional content. This content is a little bit longer.
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                                <div className="col-sm-4">
+                                    <Card className="card">
+                                        <Card.Img variant="top" src="https://www.clipartkey.com/mpngs/m/198-1988954_staff-profile-photo-facebook-blank-profile-picture-male.png" />
+                                        <Card.Body style={{ background: '#ff6200' }}>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text style={{ color: '#fff' }}>
+                                                This is a wider card with supporting text below as a natural lead-in to
+                                                additional content. This content is a little bit longer.
                                 </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    </CardDeck>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            </div>
+                        </CardDeck>
                     </ScrollAnimation>
                 </div>
             </div>
