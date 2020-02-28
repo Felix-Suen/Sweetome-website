0710af2da445179f0468d873807a123f9354d974
Felix <felix.suen2000@gmail.com>
2020-02-28 14:04:03 -0800

    about page

--- src/components/about.js
+++ src/components/about.js
@@ -10,23 +10,27 @@ class About extends React.Component {
                     <h1>Our Journey</h1><br />
                 </ScrollAnimation>
                 <ScrollAnimation animateIn="fadeIn" delay={500} animateOnce='true'>
-                    <div className="textbox">
-                        <div class="row no-gutters" style={{textAlign:'center'}}>
+                    <div className="about-box">
+                        <div class="row no-gutters" style={{ textAlign: 'center' }}>
                             <div class="col-md-4">
                                 <img src="https://images.pexels.com/photos/584399/living-room-couch-interior-room-584399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="house"></img>
                             </div>
                             <div class="col-md-8">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                                <div className="textbox">
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </ScrollAnimation>
                 <br />
                 <ScrollAnimation animateIn="fadeIn" delay={600} animateOnce='true'>
-                    <div className="textbox">
+                    <div className="about-box">
                         <div class="row no-gutters">
                             <div class="col-md-8">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                                <div className="textbox">
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                                </div>
                             </div>
                             <div class="col-md-4">
                                 <img src="https://images.pexels.com/photos/101808/pexels-photo-101808.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="key"></img>
