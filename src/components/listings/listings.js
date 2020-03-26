27c46097e8cc4dcaf6c29c194729bf0abe28100b
Felix-Suen <felix.suen2000@gmail.com>
2020-03-26 16:34:02 -0700

    filter

--- src/components/listings/listings.js
+++ src/components/listings/listings.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import firebase from './firebase';
 import Navibar from './navibar';
+import { Button, ButtonGroup, Dropdown, DropdownButton } from 'react-bootstrap';
 import './listings.css';
 
 class Listings extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            listings: []
+            listings: [],
+            filteredListings: [],
         };
     }
 
@@ -16,7 +18,8 @@ class Listings extends React.Component {
             const db = firebase.firestore()
             const data = db.collection('listings').get()
                 .then((snapshot) => {
-                    snapshot.forEach((doc) => { this.setState({ listings: [...this.state.listings, doc] }); })
+                    snapshot.forEach((doc) => { this.setState({ listings: [...this.state.listings, doc],
+                                                                filteredListings: [...this.state.filteredListings, doc] }); })
                 })
                 .catch((err) => {
                     console.log('Error getting documents', err);
@@ -26,13 +29,39 @@ class Listings extends React.Component {
         fetchData()
     }
 
+    priceFilter(low, up) {
+        var length = this.state.listings.length;
+        var temp = [];
+        this.setState({filteredListings: []});
+        console.log(this.state.filteredListings)
+        for (var i = 0; i < length; i++) {
+            if (this.state.listings[i].data().price >= low && this.state.listings[i].data().price <= up) {
+                console.log(this.state.listings[i].data().address)
+                temp.push(this.state.listings[i])
+            }
+            this.setState({filteredListings: temp})
+        }
+    }
+
+    allListings() {
+        this.setState({filteredListings: this.state.listings});
+    }
+
     render() {
         return (
             <div>
                 <Navibar />
                 <div className="grid-container">
                     <h1>All Available Listings</h1>
-                    {this.state.listings.map(e => (
+
+                    <ButtonGroup>
+                        <Button variant="orange" onClick={() => this.priceFilter(0, 1000)}>{"<"}$1000</Button>
+                        <Button variant="orange" onClick={() => this.priceFilter(1001, 2000)}>$1000 - $2000</Button>
+                        <Button variant="orange" onClick={() => this.priceFilter(2001, 100000)}>$2000+</Button>
+                        <Button variant="orange" onClick={() => this.allListings()}>All Listings</Button>
+                    </ButtonGroup>
+
+                    {this.state.filteredListings.map(e => (
                         <div className="listings-box">
                             <div class="row no-gutters" style={{ textAlign: 'center' }}>
                                 <div class="col-md-4">
@@ -42,7 +71,7 @@ class Listings extends React.Component {
                                     <div className="textbox">
                                         <h2><b>{e.data().address}</b></h2>
                                         <p>{e.data().bedroom} beds {e.data().bathroom} baths</p>
-                                        <p>{e.data().price} / Month</p>
+                                        <p>${e.data().price.toLocaleString()} / Month</p>
                                         <p>{e.data().description}</p>
                                     </div>
                                 </div>
